0441292f055796ba1c9a091ebf835d08149f7bdf
mirkocvetanoski <mirko.cvetanoski@gmail.com>
2025-03-12 14:56:57 +0100

    Styling changes

--- lib/formvalidation.ts
+++ lib/formvalidation.ts
@@ -5,8 +5,8 @@ export const RegisterFormSchema = z
     email: z.string().email({ message: 'Please enter a valid email.' }).trim(),
     password: z
       .string()
-      .min(1, { message: 'Not be empty' })
-      .min(8, { message: 'Be at least 8 characters long' })
+      .min(1, { message: 'Not be empty.' })
+      .min(8, { message: 'Be at least 8 characters long.' })
       .regex(/[a-zA-Z]/, { message: 'Contain at least one letter.' })
       .regex(/[0-9]/, { message: 'Contain at least one number.' })
       .regex(/[^a-zA-Z0-9]/, {
