57d81fb38e62af0c7a7af0f6ccf108df1c69cefb
mirkocvetanoski <mirko.cvetanoski@gmail.com>
2025-03-20 09:25:54 +0100

    mongo database added

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
 import MainLayout from '@/components/selfui/MainLayout';
+import connectDB from '@/config/database';
 
-export default function Home() {
+const HomePage = async () => {
+  await connectDB();
+
   return (
     <MainLayout>
       <div className="h-[calc(100vh-100px)]">HomePage</div>
     </MainLayout>
   );
-}
+};
+
+export default HomePage;
