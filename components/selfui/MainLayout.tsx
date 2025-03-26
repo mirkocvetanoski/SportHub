7b6a65993e6fab6707f57cbfc7480f8cc541bdea
mirkocvetanoski <mirko.cvetanoski@gmail.com>
2025-03-26 13:36:40 +0100

    user added

--- components/selfui/MainLayout.tsx
+++ components/selfui/MainLayout.tsx
@@ -1,25 +1,39 @@
 import Navbar from './Navbar';
-
 import { ColorModeProvider } from '@/components/ui/color-mode';
-import { SimpleGrid, Theme } from '@chakra-ui/react';
+import User from '@/models/User';
+import { SimpleGrid } from '@chakra-ui/react';
 import { cookies } from 'next/headers';
+import jwt from 'jsonwebtoken';
+import { ReactNode } from 'react';
 
-const MainLayout = async (props: { children: React.ReactNode }) => {
-  const { children } = props;
+interface MainLayoutProps {
+  children: ReactNode;
+}
 
+interface JwtPayload {
+  userId: string;
+}
+
+const MainLayout = async ({ children }: MainLayoutProps) => {
   const cookieStore = await cookies();
-  const token = cookieStore.get('SPORTSHUB_ACCESS_TOKEN');
+  const token = cookieStore.get('SPORTSHUB_ACCESS_TOKEN')?.value;
 
-  console.log(token);
+  if (token) {
+    try {
+      const decoded = jwt.verify(token, process.env.JWT_SECRET!) as JwtPayload;
+      const user = await User.findById(decoded.userId);
+      console.log(user);
+    } catch (error) {
+      console.error('Invalid token:', error);
+    }
+  }
 
   return (
     <ColorModeProvider forcedTheme="dark">
-      <Theme colorScheme="dark">
-        <SimpleGrid width="100vw">
-          <Navbar />
-          {children}
-        </SimpleGrid>
-      </Theme>
+      <SimpleGrid width="100vw">
+        <Navbar />
+        {children}
+      </SimpleGrid>
     </ColorModeProvider>
   );
 };
