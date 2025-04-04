7deac0c5541298a648117580cd6b092356238172
mirkocvetanoski <mirko.cvetanoski@gmail.com>
2025-04-04 09:14:53 +0200

    Logo changes

--- components/selfui/Logo.tsx
+++ components/selfui/Logo.tsx
@@ -1,8 +1,3 @@
-import {
-  GRADIENT_COLOR1,
-  GRADIENT_COLOR2,
-  GRADIENT_COLOR3,
-} from '@/lib/constants';
 import Link from 'next/link';
 
 interface ChildComponentProps {
@@ -25,7 +20,7 @@ const Logo: React.FC<ChildComponentProps> = ({ size }) => {
   return (
     <Link
       href="/"
-      className={`bg-gradient-to-r from-${GRADIENT_COLOR1} via-${GRADIENT_COLOR2} to-${GRADIENT_COLOR3} bg-clip-text ${getTextSize(size)} font-bold uppercase text-transparent`}
+      className={`bg-gradient-to-r from-[#22c1c3] via-[#22c1c3] to-[#fdbb2d] bg-clip-text ${getTextSize(size)} font-bold uppercase text-transparent`}
     >
       SportsHub
     </Link>
