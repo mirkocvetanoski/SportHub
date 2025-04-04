d07336c235fe13d49aec4a6a4aa242f1e9769fd3
mirkocvetanoski <mirko.cvetanoski@gmail.com>
2025-04-04 08:57:04 +0200

    Logo changes

--- components/selfui/Logo.tsx
+++ components/selfui/Logo.tsx
@@ -9,11 +9,23 @@ interface ChildComponentProps {
   size: number;
 }
 
+const getTextSize = (size: number) => {
+  const sizes = {
+    1: 'text-xl',
+    2: 'text-2xl',
+    3: 'text-3xl',
+    4: 'text-4xl',
+    5: 'text-5xl',
+    6: 'text-6xl',
+  };
+  return sizes[size as keyof typeof sizes] || 'text-2xl';
+};
+
 const Logo: React.FC<ChildComponentProps> = ({ size }) => {
   return (
     <Link
       href="/"
-      className={`bg-gradient-to-r from-[${GRADIENT_COLOR1}] via-[${GRADIENT_COLOR2}] to-[${GRADIENT_COLOR3}] bg-clip-text text-${size}xl font-bold uppercase text-transparent`}
+      className={`bg-gradient-to-r from-${GRADIENT_COLOR1} via-${GRADIENT_COLOR2} to-${GRADIENT_COLOR3} bg-clip-text ${getTextSize(size)} font-bold uppercase text-transparent`}
     >
       SportsHub
     </Link>
