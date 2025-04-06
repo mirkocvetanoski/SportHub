f2d5cf451a77fa3de4242671ffc06f0926b1e9a6
mirkocvetanoski <mirko.cvetanoski@gmail.com>
2025-04-06 21:17:39 +0200

    Active competition added

--- components/selfui/main/MainCompetitions.tsx
+++ components/selfui/main/MainCompetitions.tsx
@@ -5,6 +5,7 @@ import { Flex, Text, Icon, ClientOnly } from '@chakra-ui/react';
 import { MdSports } from 'react-icons/md';
 
 import { useColorModeValue } from '@/components/ui/color-mode';
+import { useState } from 'react';
 
 type CompetitionName = keyof typeof competitionsIcons;
 
@@ -17,6 +18,8 @@ const MainCompetitions: React.FC<CompetitionsProps> = ({ competitions }) => {
   const hoverTextColor = useColorModeValue('gray.900', 'gray.400');
   const borderColor = useColorModeValue('orange.500', 'yellow.500');
 
+  const [active, setActive] = useState<string>('Football');
+
   return competitions.map((competition, i) => {
     const IconComponent =
       (competitionsIcons[
@@ -26,18 +29,21 @@ const MainCompetitions: React.FC<CompetitionsProps> = ({ competitions }) => {
     return (
       <ClientOnly key={i}>
         <Flex
+          data-competition={competition}
           align="center"
           px={1}
           gap={2}
           cursor="pointer"
           height="100%"
-          color={textColor}
+          color={active === competition ? borderColor : textColor}
           borderBottom="2px solid"
-          borderColor="transparent"
+          borderColor={active === competition ? borderColor : 'transparent'}
           transition="border-color 0.2s"
           _hover={{
-            color: hoverTextColor,
-            borderColor: borderColor,
+            color: active === competition ? '' : hoverTextColor,
+          }}
+          onClick={e => {
+            setActive(e.currentTarget.dataset.competition || 'Football');
           }}
         >
           <Icon as={IconComponent} boxSize={5} />
