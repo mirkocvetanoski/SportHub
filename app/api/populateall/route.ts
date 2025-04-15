40238506a1f738ec14a10bc55d9b513d9f9c4adb
mirkocvetanoski <mirko.cvetanoski@gmail.com>
2025-04-15 12:19:36 +0200

    new api route added for get football leagues and new cron

--- app/api/populateall/route.ts
+++ app/api/populateall/route.ts
@@ -1,17 +1,22 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
+import { NextRequest, NextResponse } from 'next/server';
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
+export async function POST(_req: NextRequest) {
+  console.log(_req);
   try {
     await fetch(`${process.env.NEXT_PUBLIC_API_DOMAIN}/competitions/populate`);
     await fetch(`${process.env.NEXT_PUBLIC_API_DOMAIN}/countries/populate`);
     await fetch(
       `${process.env.NEXT_PUBLIC_API_DOMAIN}/footballleagues/populate`
     );
-    res.status(200).json({ message: 'All jobs completed' });
+
+    return NextResponse.json(
+      { message: 'All jobs completed' },
+      { status: 200 }
+    );
   } catch (error) {
-    res.status(500).json({ error: 'Something went wrong', details: error });
+    return NextResponse.json(
+      { error: 'Something went wrong', details: String(error) },
+      { status: 500 }
+    );
   }
 }
